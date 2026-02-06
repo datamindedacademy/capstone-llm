41319b4313e95e930e165a385815d8daa7cafb60
nielsc <niels.claeys@gmail.com>
2026-02-06 15:41:36 +0100

    Support Airflow 3.

--- dags/stackoverflow_etl.py
+++ dags/stackoverflow_etl.py
@@ -1,4 +1,5 @@
 from airflow import DAG
+from datetime import datetime, timedelta
 from conveyor.operators import ConveyorSparkSubmitOperatorV2, ConveyorContainerOperatorV2
 from datetime import timedelta
 from airflow.utils import dates
@@ -7,7 +8,7 @@ from airflow.utils import dates
 default_args = {
     "owner": "Conveyor",
     "depends_on_past": False,
-    "start_date": dates.days_ago(2),
+    "start_date": datetime.now() - timedelta(days=2),
     "email": [],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -19,7 +20,7 @@ default_args = {
 dag = DAG(
     "stackoverflow_etl",
     default_args=default_args,
-    schedule_interval="@daily",
+    schedule="@daily",
     max_active_runs=1,
 )
 
@@ -35,10 +36,8 @@ ingest_task = ConveyorContainerOperatorV2(
 clean_task = ConveyorContainerOperatorV2(
     dag=dag,
     task_id="clean",
-    num_executors="1",
     instance_type="mx.medium",
     aws_role="capstone_conveyor_llm",
-    spark_main_version=3,
     cmds=["python3", "-m", "stackoverflowetl.tasks.clean"],
     arguments=["-e", "{{ macros.conveyor.env() }}"]
 )
